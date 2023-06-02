10f937196e5903f970c6390a2094ecdef2303ea3
Sergej Sakac <sakacszergej@gmail.com>
2023-06-02 19:23:45 +0200

    identity contract storage values

--- contracts/identity/lib.rs
+++ contracts/identity/lib.rs
@@ -1,14 +1,51 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use ink::storage::traits::StorageLayout;
+
+/// Each identity will be associated with a unique identifier called `IdentityNo`.
+pub type IdentityNo = u64;
+
+/// We want to keep the address type very generic since we want to support any
+/// address format. We won't actually keep the addresses in the contract itself.
+/// Before storing them, we'll encrypt them to ensure privacy.
+pub type Address = Vec<u8>;
+
+/// Used to represent any blockchain in the Polkadot, Kusama or Rococo network.
+#[derive(scale::Encode, scale::Decode, Debug)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo, StorageLayout))]
+pub enum Network {
+	/// Polkadot network
+	Polkadot(String),
+	/// Kusama network
+	Kusama(String),
+	/// Rococo network
+	Rococo(String),
+}
+
+#[derive(scale::Encode, scale::Decode, Debug)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo, StorageLayout))]
+pub struct IdentityInfo {
+	/// Each address is associated with a specific blockchain.
+	addresses: Vec<(Network, Address)>,
+}
+
 #[ink::contract]
 mod identity {
+	use super::*;
+	use ink::storage::Mapping;
+
 	#[ink(storage)]
-	pub struct Identity {}
+	#[derive(Default)]
+	pub struct Identity {
+		number_to_identity: Mapping<IdentityNo, IdentityInfo>,
+		owner_of: Mapping<IdentityNo, AccountId>,
+		identity_of: Mapping<AccountId, IdentityNo>,
+	}
 
 	impl Identity {
 		#[ink(constructor)]
 		pub fn new() -> Self {
-			Identity {}
+			Default::default()
 		}
 
 		#[ink(message)]
