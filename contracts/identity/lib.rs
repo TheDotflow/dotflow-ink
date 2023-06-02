505fe72a2917ba44e95016c35842262b29ff3d07
cuteolaf <oliverlim818@gmail.com>
2023-06-02 21:20:28 +0200

    add boilerplate for required functions

--- contracts/identity/lib.rs
+++ contracts/identity/lib.rs
@@ -23,6 +23,23 @@ pub struct IdentityInfo {
 	addresses: Vec<(Network, Address)>,
 }
 
+impl IdentityInfo {
+	/// Adds an address for the given network
+	pub fn add_address(network: Network, address: Address) {
+		// TODO:
+	}
+
+	/// Updates the address of the given network
+	pub fn update_address(network: Network, new_address: Address) {
+		// TODO:
+	}
+
+	/// Remove address of the given network
+	pub fn remove_address(network: Network) {
+		// TODO:
+	}
+}
+
 #[ink::contract]
 mod identity {
 	use super::*;
@@ -34,6 +51,7 @@ mod identity {
 		number_to_identity: Mapping<IdentityNo, IdentityInfo>,
 		owner_of: Mapping<IdentityNo, AccountId>,
 		identity_of: Mapping<AccountId, IdentityNo>,
+		total_identity: u64,
 	}
 
 	impl Identity {
@@ -42,9 +60,52 @@ mod identity {
 			Default::default()
 		}
 
+		// TODO: remove this
 		#[ink(message)]
 		pub fn foo(&self) -> u32 {
 			42
+		}
+
+		#[ink(message)]
+		/// Create an identity and returns the `IdentityNo`
+		/// A user can only create one identity
+		pub fn create_identity(&self) -> IdentityNo {
+			// TODO: Check if the caller already owns an identity
+
+			// Generate a new IdentityNo
+			let new_id = self.total_identity;
+
+			// TODO: initialize IdentityInfo
+			// TODO: Associate the newly created IdentityInfo with required mappings
+
+			// Increase the number of identities
+			self.total_identity = self.total_identity + 1;
+
+			new_id
+		}
+
+		#[ink(message)]
+		/// Adds an address for a given network
+		pub fn add_address(&self, network: Network, address: Address) {
+			// TODO:
+		}
+
+		#[ink(message)]
+		/// Updates the address of the given network
+		pub fn update_address(&self, network: Network, address: Address) {
+			// TODO:
+		}
+
+		#[ink(message)]
+		/// Removes the address by network
+		pub fn remove_address(&self, network: Network) {
+			// TODO:
+		}
+
+		#[ink(message)]
+		/// Removes an identity
+		pub fn remove_identity(&self, identity_no: IdentityNo) {
+			// TODO:
 		}
 	}
 
